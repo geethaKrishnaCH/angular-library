c521dbc87d785fab0bc816492500e7e12ab4e90d
geetha@jr <chgeethakrishna@gmail.com>
2022-11-09 16:05:09 +0530

    adfad

--- projects/commons/src/lib/commons.module.ts
+++ projects/commons/src/lib/commons.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
-import { BioComponent } from './bio/bio.component';
+import { BioComponent} from './bio/bio.component';
 
 
 
