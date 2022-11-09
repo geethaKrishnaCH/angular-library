f6ea4910aa371b63903b8bd80d91415045eb9160
geetha@jr <chgeethakrishna@gmail.com>
2022-11-09 15:56:09 +0530

    adding a new component

---
+++ projects/commons/src/lib/bio/bio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-bio',
+  templateUrl: './bio.component.html',
+  styleUrls: ['./bio.component.css']
+})
+export class BioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/commons/src/lib/commons.module.ts
+++ projects/commons/src/lib/commons.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
+import { BioComponent } from './bio/bio.component';
 
 
 
 @NgModule({
   declarations: [
   
-    IntroComponent
+    IntroComponent,
+       BioComponent
   ],
   imports: [
   ],
